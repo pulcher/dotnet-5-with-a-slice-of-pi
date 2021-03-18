bb913b14e2e729c4baa5a64c3c40d20baca1a567
Harold Pulcher <pulcher@pulcher.biz>
2021-03-17 21:49:28 -0500

    added some blinky leds

--- examples/pushBlink/Program.cs
+++ examples/pushBlink/Program.cs
@@ -10,7 +10,9 @@ namespace pushBlink
     {
         private static GpioController controller;
         private static int buttonPin = 5;
-        private static int LEDPin = 6;
+        private static int constantBlinkPin = 16;
+        private static bool contanstState = false;
+        private static int activatedBlinkPin = 6;
 
         static void Main(string[] args)
         {
@@ -25,9 +27,23 @@ namespace pushBlink
             while (!done)
             {
                 var status = controller.Read(buttonPin);
-                
+
                 if (status == PinValue.Low)
+                {
                     System.Console.WriteLine("Button Pressed!!!");
+                    controller.Write(activatedBlinkPin, PinValue.High);
+                }
+                else 
+                {
+                    controller.Write(activatedBlinkPin, PinValue.Low);
+                }
+
+                if (contanstState)
+                    controller.Write(constantBlinkPin, PinValue.High);
+                else
+                    controller.Write(constantBlinkPin, PinValue.Low);
+
+                contanstState = !contanstState;
 
                 Thread.Sleep(100);
             }
@@ -40,7 +56,12 @@ namespace pushBlink
             Console.WriteLine("Setting up GPIO...");
             controller = new GpioController(0, new RaspberryPi3Driver());
 
+            Console.WriteLine("Initiating pullup on button....");
             controller.OpenPin(buttonPin, PinMode.InputPullUp);
+
+            Console.WriteLine("Initializing LED indicators...");
+            controller.OpenPin(constantBlinkPin, PinMode.Output);
+            controller.OpenPin(activatedBlinkPin, PinMode.Output);
         }
     }
 }
